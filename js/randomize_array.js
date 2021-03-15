c17a9bbf43f4ab536dae5794643ad733f919f9d0
Rushabh Doshi <rushabh.kd@somaiya.edu>
2021-03-15 13:20:34 +0530

    Added range slider for speed and size of array

--- js/randomize_array.js
+++ js/randomize_array.js
@@ -2,21 +2,14 @@ var root=document.getElementById("arr-elements")
 var div_array=[]
 var div_size=[]
 var array_size=8
-var margin_size=0.1
+var margin_size=0.2
 function randomize() {
-    array_size = parseInt(document.getElementById("arraySizeText").value) || 10
-    console.log(array_size)
-    if(array_size > 400 || array_size<5)
-        return
-    // array_size = array_size || 10
-    
-    
     console.log(array_size)
     root.innerHTML=""
     div_array=[]
     div_size=[]
     for(i=0;i<array_size;i++){
-        var height = Math.round(Math.random()*100)+5
+        var height =  Math.random()*100
         var bar = document.createElement("div")
         root.appendChild(bar)
         div_array.push(bar);
--- js/util.js
+++ js/util.js
@@ -1,15 +1,14 @@
-speed=1
-var delay_time=100/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
-var c_delay=0;//This is updated ov every div change so that visualization is visible.
+var delay 
 function  update_div(element,height,color,speed=(10000/array_size)){
-
+    delay = delay??speed
     return new Promise(resolve => setTimeout(()=> {        
         element.style="margin: 0% "+margin_size+"%; "+
         "background-color: "+color+"; "+
         "width: "+(100/array_size)+"%; "+
         "height: "+height+"%; "
+        console.log(delay)
         resolve("success")
-    },speed))
+    },delay))
   /*  window.setTimeout(function(){
         element.style="margin: 0% "+margin_size+"%; "+
         "background-color: "+color+"; "+
